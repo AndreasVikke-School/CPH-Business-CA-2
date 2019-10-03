ddc54bb7d2c5da160993e1e07dfd26c5d9de02a8
Andreas Vikke <vikkedesign@gmail.com>
2019-10-03 14:33:17 +0200

    Added table gen

--- CA2-Frontend/src/index.js
+++ CA2-Frontend/src/index.js
@@ -1,4 +1,44 @@
 import 'bootstrap/dist/css/bootstrap.css'
 
+window.onload = () => {
+    document.getElementById("container").append(createTable([{"firstname" : "Andreas", "lastname" : "Vikke"},
+    {"firstname" : "Emil", "lastname" : "Svensmark"}]));
+}
 
 
+function createTable(array) {
+    if (!Array.isArray(array))
+        array = [array];
+
+    var table = document.createElement("table");
+    table.classList.add("table");
+
+    var tHead = table.createTHead();
+    tHead.classList.add("thead-dark");
+
+    var tRow = tHead.insertRow(0);
+    Object.keys(array[0]).map(function (key, index) {
+        tRow.insertCell(index).outerHTML = "<th>" + key.charAt(0).toUpperCase() + key.slice(1); + "</th>";
+    });
+
+    var tBody = table.createTBody();
+    tBody.addEventListener("click", (e) => { tBodyClickHandler(e) });
+    array.map(function (obj, index) {
+        var tBRow = tBody.insertRow(index);
+        Object.keys(obj).map(function (key, index) {
+            if(typeof obj[key] === 'object') {
+                var objS = [];
+                Object.keys(obj[key]).map(k => {
+                    if(k != "id")
+                        objS.push(obj[key][k])
+                });
+                tBRow.insertCell(index).innerHTML = objS.join(", ");
+            }
+            else
+                tBRow.insertCell(index).innerHTML = obj[key];
+            
+        });
+    });
+
+    return table;
+}

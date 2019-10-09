bca66cd2f648da7e92f6e486aeb80b106c9c26c3
AndreasVikke <vikkedesign@gmail.com>
2019-10-09 11:47:02 +0200

    Added CORS xD

--- CA2-Backend/src/main/java/rest/ApplicationConfig.java
+++ CA2-Backend/src/main/java/rest/ApplicationConfig.java
@@ -49,6 +49,7 @@ public class ApplicationConfig extends Application {
         resources.add(rest.AddressResource.class);
         resources.add(rest.HobbyResource.class);
         resources.add(rest.PhoneResource.class);
+        resources.add(rest.cors.CorsRequestFilter.class);
     }
 
 }
